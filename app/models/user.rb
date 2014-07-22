54d5869e83a4912447c8dc0612dfbbfcf3f4d91e
James Lieu <j.lieu888@gmail.com>
2014-07-22 18:09:08 +0100

    Added image, update readme

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,6 @@ class User < ActiveRecord::Base
 has_many :logs
 
   def self.authenticate(password)
-    User.find_by_password(password)#match_password(password)
+    User.find_by_password(password)
   end
-
-  # def logged_in?
-  #   !!session[:user_id]
-  # end
 end
-
-
